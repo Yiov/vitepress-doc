e40edccf0b16e366cab52f2098c8aa5ac7a7a388
许大仙 <1900919313@qq.com>
2025-02-19 07:05:39 +0000

    鼠标粒子效果

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -28,6 +28,8 @@ import MyLayout from "./components/MyLayout.vue" //视图过渡
 import backtotop from "./components/backtotop.vue" //返回顶部
 import notice from "./components/notice.vue" //公告
 import fluidborder from "./components/fluidborder.vue" //流体边框仅用于演示
+import MouseClick from "./components/MouseClick.vue"
+import MouseFollower from "./components/MouseFollower.vue"
 
 // 不蒜子
 import { inBrowser } from 'vitepress'
@@ -48,6 +50,8 @@ export default {
     app.component('ArticleMetadata' , ArticleMetadata) //字数阅读时间
     app.component('Linkcard' , Linkcard) //链接卡片
     app.component('fluidborder' , fluidborder) //流体边框仅用于演示
+    app.component('MouseClick', MouseClick) //鼠标跟随组件
+    app.component('MouseFollower', MouseFollower) //鼠标跟随组件
 
     // 不蒜子
     if (inBrowser) {
@@ -55,7 +59,7 @@ export default {
       router.onBeforeRouteChange = () => {
         NProgress.start() // 开始进度条
       }
-      router.onAfterRouteChanged = () => {
+      router.onAfterRouteChange = () => {
          busuanzi.fetch()
          NProgress.done() // 停止进度条
        }
